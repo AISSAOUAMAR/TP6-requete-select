ebeea2c40c387859e63e1d72f12fd5c0b889ad4b
ESSARRAJ <essarraj.fouad@gmail.com>
2014-12-31 10:08:42 +0100

    init code

---
+++ ExecutionRequeteInsert/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace ExecutionRequeteInsert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connexionString, requete;
+            connexionString = @"Data Source=localhost;Initial Catalog=GestionStagiaires;User= 'sa'; password = 'admintp4'";
+            requete = "SELECT * FROM Stagiaires";
+            SqlConnection connexion = new SqlConnection(connexionString);
+            SqlCommand commande = connexion.CreateCommand();
+            commande.CommandText = requete;
+            connexion.Open();
+            SqlDataReader lire = commande.ExecuteReader();
+            while (lire.Read())
+            {
+                Console.WriteLine("ID : {0} | Nom : {1} | Prenom : {2}", lire.GetInt32(0), lire.GetString(1), lire["Prenom"]);
+            }
+            connexion.Close();
+            connexion.Dispose();
+            Console.ReadLine();
+        }
+    }
+}
