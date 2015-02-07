e51859bf091195d4136a6594cff6f949d7ea2be6
AISSAOUAMAR <aissa_tanger93@hotmail.fr>
2015-02-07 17:24:55 +0100

    commit tp6

--- ExecutionRequeteInsert/Program.cs
+++ ExecutionRequeteInsert/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 using System.Data.SqlClient;
-namespace ExecutionRequeteInsert
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace AppStagiaire {
+    class Program {
+        static void Main(string[] args) 
+          { 
             string connexionString, requete;
-            connexionString = @"Data Source=localhost;Initial Catalog=GestionStagiaires;User= 'sa'; password = 'admintp4'";
+            connexionString = @"Data Source=AISSA-PC\SQLEXPRESS;Initial Catalog=GestionStagiaires;Integrated Security=True";
             requete = "SELECT * FROM Stagiaires";
             SqlConnection connexion = new SqlConnection(connexionString);
             SqlCommand commande = connexion.CreateCommand();
             commande.CommandText = requete;
             connexion.Open();
             SqlDataReader lire = commande.ExecuteReader();
-            while (lire.Read())
-            {
-                Console.WriteLine("ID : {0} | Nom : {1} | Prenom : {2}", lire.GetInt32(0), lire.GetString(1), lire["Prenom"]);
+            while (lire.Read()) {
+                Console.WriteLine(
+                              "ID : {0} | Nom : {1} | Prenom : {2}" , lire.GetInt32(0), lire.GetString(1),lire["Prenom"]);
             }
             connexion.Close();
             connexion.Dispose();
